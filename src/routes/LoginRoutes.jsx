d3425976a3b945a52ba88fd4741857f641691e27
dgagan2 <andgag@duck.com>
2023-10-28 12:16:34 -0500

    Se agrega la ruta NotFound

--- src/routes/LoginRoutes.jsx
+++ src/routes/LoginRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/home/Home'
+import NotFound from '../pages/notFound/NotFound'
 
 const LoginRoutes = () => {
   return (
     <Routes>
       <Route path='/home' element={<Home />} />
+      <Route path='/*' element={<NotFound />} />
     </Routes>
 
   )
