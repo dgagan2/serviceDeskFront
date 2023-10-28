092ba4756eb00416342f34a61587b9bb40b03193
dgagan2 <andgag@duck.com>
2023-10-28 12:17:05 -0500

    Si valida si el usuario esta logueado para saber donde redirigir

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './notFound.css'
 import { Link } from 'react-router-dom'
+import { useUserContext } from '../../hooks/UseUserContext'
 
 const NotFound = () => {
+  const { isLoggin } = useUserContext()
   return (
     <main className='container-page-not-found'>
       <div>
         <p className='status-code'>404</p>
         <h1>Page not found</h1>
         <p className='message'>Sorry, we couldn’t find the page you’re looking for.</p>
-        <Link to='/login' className='btn btn-primary'>
+        <Link to={isLoggin ? '/home' : '/login'} className='btn btn-primary'>
           Go back home
         </Link>
 
