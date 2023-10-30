443c5aa22e4000ee00d05e61e56fecfc2b230629
dgagan2 <andgag@duck.com>
2023-10-30 00:12:02 -0500

    boton para eliminar un objeto

--- src/components/buttons/DeleteButton.jsx
+++ src/components/buttons/DeleteButton.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 import iconDelete from '../../assets/iconDelete.png'
-const deleteButton = () => {
+const DeleteButton = ({ Delete, id }) => {
   const deleteObject = () => {
-
+    Delete(id)
   }
   return (
     <button id='btn-delete' onClick={() => deleteObject()}><img src={iconDelete} alt='button to delete' /></button>
   )
 }
 
-export default deleteButton
+export default DeleteButton
